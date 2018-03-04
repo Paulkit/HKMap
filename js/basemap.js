ba6da504366d2f39097a4dafdfaeafd5ad37622e
Paul Chu <paulkit41@gmail.com>
2018-03-04 08:54:54 +0800

    color changed

--- js/basemap.js
+++ js/basemap.js
@@ -1,46 +1,44 @@
- 
-var infowindow;
-var map;
-var myLatLng = [];
-
-var district = [
   
-  "https://raw.githubusercontent.com/Paulkit/HKMap/master/Central%20and%20Western.kml",
-  "https://raw.githubusercontent.com/Paulkit/HKMap/master/Eastern.kml",
-  "https://raw.githubusercontent.com/Paulkit/HKMap/master/Islands.kml",
-  "https://raw.githubusercontent.com/Paulkit/HKMap/master/Kowloon%20City.kml",
-  "https://raw.githubusercontent.com/Paulkit/HKMap/master/Kwai%20Tsing.kml",
-  "https://raw.githubusercontent.com/Paulkit/HKMap/master/Kwun%20Tong.kml",
-  "https://raw.githubusercontent.com/Paulkit/HKMap/master/North.kml",
-  "https://raw.githubusercontent.com/Paulkit/HKMap/master/Sai%20Kung.kml",
-  "https://raw.githubusercontent.com/Paulkit/HKMap/master/Sha%20Tin.kml",
-  "https://raw.githubusercontent.com/Paulkit/HKMap/master/Sham%20Shui%20Po.kml",
-  "https://raw.githubusercontent.com/Paulkit/HKMap/master/Southern.kml",
-  "https://raw.githubusercontent.com/Paulkit/HKMap/master/Tai%20Po.kml",
-  "https://raw.githubusercontent.com/Paulkit/HKMap/master/Tsuen%20Wan.kml",
-  "https://raw.githubusercontent.com/Paulkit/HKMap/master/Tuen%20Mun.kml",
-  "https://raw.githubusercontent.com/Paulkit/HKMap/master/Wan%20Chai.kml",
-  "https://raw.githubusercontent.com/Paulkit/HKMap/master/Wong%20Tai%20Sin.kml",
-  "https://raw.githubusercontent.com/Paulkit/HKMap/master/Yau%20Tsim%20Mong.kml",
-  "https://raw.githubusercontent.com/Paulkit/HKMap/master/Yuen%20Long.kml"
+ 
+ 
+// Function Prevent User Go outside of Hong Kong
+/**
+ *  Listen drag event
+ * 
+ * 
+ * 
+ */
+ // Listen for the dragend event
+   baseCenterPoint = new google.maps.LatLng(22.362536, 114.118785);
    
-
- ];
-
- 
-      
- 
-var array = new Array;
-
-for (i = 0; i < district.length; i++) {
-    var kml =    new google.maps.KmlLayer({
-                
-                        url: district[i],
-                        map: map
-    });
-
-    array.push( kml);
+  
+ google.maps.event.addListener(map, 'center_changed', function() {
     
+ 
+    if(!arePointsNear(map.getCenter(), baseCenterPoint,40)){
+     
+       map.panTo(baseCenterPoint);
+   //  animateMapZoomTo(10);
+    }
+   
+         
+      });
+
+ 
+    /**
+ *  detect center point within Hong Kong region
+ * 
+ * 
+ * 
+ */  
+function arePointsNear(checkPoint, centerPoint, km) {
+ 
+  var ky = 40000 / 360;
+  var kx = Math.cos(Math.PI * centerPoint.lat() / 180.0) * ky;
+ 
+  var dx = Math.abs(centerPoint.lng() - checkPoint.lng()) * kx;
+  var dy = Math.abs(centerPoint.lat() - checkPoint.lat()) * ky;
+  return (Math.sqrt(dx * dx + dy * dy) <= km);
 }
 
  
